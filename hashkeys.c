09b8a9a3b9a50c7835bf7e1adbacfc73e161c4ec
PurrGod <srinid369@gmail.com>
2025-08-24 15:17:59 -0700

    created an initialization function to initialize hashkeys such as piecekeys, castleperems and en passant.

--- hashkeys.c
+++ hashkeys.c
@@ -1,10 +1,45 @@
 #include "definitions.h"
+#include "stdlib.h"
 
 //generate position keys to create a unique key for each position
 //this is used to check for repeated states
 //common technique is to XOR all te keys
 
-U64 PieceKeys[13][64];
+U64 PieceKeys[13][64]; // keys for each piece wPawn to bKing 0 = empty, 1 - 13
 U64 Sidekey;
 U64 CastleKeys[16];
+U64 EPFileKey[8]; // enpassant file key
 
+// generate 64 bit random integer
+// rand()
+
+// initialize all the hashing keys first
+static void init_hash_keys() {
+    // first initialize piece keys
+    for (int piece = wPawn; piece <= bKing; piece++){
+        for (int sq = 0; p < 64; p++){
+            PieceKeys[piece][sq] = rand();
+        }
+    }
+
+    // side key
+    Sidekey = rand();
+
+    // castlekeys, there are 16 combinations since 4 bits
+    for (int i = 0; i < 16; i++){
+        CastleKeys[i] = rand();
+    }
+
+    // unique number for enpassant
+    for (int f = 0; f < 8; f++) {
+        EPFileKey[f] = rand();
+    }
+}
+
+U64 zobrist_hashing(Bitboards * bb) {
+    // we take the current boardstate.
+    // create a random 64 bit integer.
+    U64 key = {0};
+
+
+}
