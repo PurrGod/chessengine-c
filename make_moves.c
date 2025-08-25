b5e124d1ea9aea84e7101319040a637549741708
PurrGod <srinid369@gmail.com>
2025-08-24 20:09:16 -0700

    Started researching about make move function

---
+++ make_moves.c
@@ -0,0 +1,9 @@
+#include "make_moves.h"
+#include "definitions.h"
+
+
+void make_move(Bitboards * bb, moveList * list) {
+    // loop should be what?
+    // length of the moveList?
+    int moveCount = list->count;
+}
