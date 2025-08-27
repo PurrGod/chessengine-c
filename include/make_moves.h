37f635d9411f5553d6e547cd926a29b5b071edf9
PurrGod <srinid369@gmail.com>
2025-08-26 20:59:45 -0700

    Added unmake function. Can finally move to perft function

--- include/make_moves.h
+++ include/make_moves.h
@@ -1,3 +1,4 @@
 #include "definitions.h"
 
 void make_move(Bitboards * bb, int move);
+void unmake_move(Bitboards * bb);
--- src/make_moves.c
+++ src/make_moves.c
@@ -240,3 +240,112 @@ void make_move(Bitboards * bb, int m) {
     bb->side ^= 1;
     bb->posKey ^= Sidekey;
 }
+
+
+// reverse all changes made from a move integer using bb.history
+// Add this function to make_moves.c
+
+void unmake_move(Bitboards *bb) {
+    // Decrement the history ply to point to the last move made
+    bb->hisPly--;
+    bb->ply--;
+
+    // --- PHASE 1: RESTORE PREVIOUS STATE ---
+    // Restore all the board state variables from the history array.
+
+    int move = bb->history[bb->hisPly].move;
+    bb->side = bb->history[bb->hisPly].side;
+    bb->fiftyMove = bb->history[bb->hisPly].fiftyMove;
+    bb->enPas = bb->history[bb->hisPly].enPas;
+    bb->castlePerm = bb->history[bb->hisPly].castlePerm;
+    bb->posKey = bb->history[bb->hisPly].posKey;
+
+    // extract previous move details
+    int from = MOVE_FROM(move);
+    int to = MOVE_TO(move);
+    int captured = MOVE_CAPTURED(move);
+    int promoted = MOVE_PROMOTION(move);
+
+    // REVERSE THE PIECE MOVEMENT
+    
+    // Identify the piece that moved. If it was a promotion, the piece on the 'to'
+    // square is the promoted piece, but the piece we need to move back is a pawn.
+    int moving_piece;
+    if (promoted != EMPTY) {
+        moving_piece = (bb->side == WHITE) ? wPawn : bPawn;
+        // Remove the promoted piece from the 'to' square
+        U64 *promoted_bb_ptr = get_piece_bitboard(bb, promoted);
+        clearbit(*promoted_bb_ptr, to);
+    } else {
+        // We are moving back the back so the moving piece is "to" square
+        moving_piece = get_piece_on_square(bb, to, bb->side);
+    }
+
+    // Move the piece back from 'to' to 'from'
+    U64 *piece_bb_ptr = get_piece_bitboard(bb, moving_piece);
+    setbit(*piece_bb_ptr, from);
+    clearbit(*piece_bb_ptr, to);
+
+    // Update the bitboards
+    setbit(bb->occupied[bb->side], from);
+    clearbit(bb->occupied[bb->side], to);
+    setbit(bb->all_pieces, from);
+    clearbit(bb->all_pieces, to);
+
+    // UNDO SPECIAL MOVES
+
+    // If it was a capture, put the captured piece back on the 'to' square
+    if (captured != EMPTY) {
+        // For en passant, the captured piece is NOT on the 'to' square
+        if (move & MOVE_IS_ENPASSANT) {
+            int cap_sq = (bb->side == WHITE) ? to - 8 : to + 8;
+            setbit(bb->pawns[!bb->side], cap_sq);
+            setbit(bb->occupied[!bb->side], cap_sq);
+            setbit(bb->all_pieces, cap_sq);
+        } else {
+            // For a standard capture
+            U64 *captured_bb_ptr = get_piece_bitboard(bb, captured);
+            setbit(*captured_bb_ptr, to);
+            setbit(bb->occupied[!bb->side], to);
+            setbit(bb->all_pieces, to);
+        }
+    }
+
+    // If it was a castle, move the rook back to its home square
+    if (move & MOVE_IS_CASTLE) {
+        switch (to) {
+            case G1: // White Kingside
+                setbit(bb->rooks[WHITE], H1);
+                clearbit(bb->rooks[WHITE], F1);
+                setbit(bb->occupied[WHITE], H1);
+                clearbit(bb->occupied[WHITE], F1);
+                setbit(bb->all_pieces, H1);
+                clearbit(bb->all_pieces, F1);
+                break;
+            case C1: // White Queenside
+                setbit(bb->rooks[WHITE], A1);
+                clearbit(bb->rooks[WHITE], D1);
+                setbit(bb->occupied[WHITE], A1);
+                clearbit(bb->occupied[WHITE], D1);
+                setbit(bb->all_pieces, A1);
+                clearbit(bb->all_pieces, D1);
+                break;
+            case G8: // Black Kingside
+                setbit(bb->rooks[BLACK], H8);
+                clearbit(bb->rooks[BLACK], F8);
+                setbit(bb->occupied[BLACK], H8);
+                clearbit(bb->occupied[BLACK], F8);
+                setbit(bb->all_pieces, H8);
+                clearbit(bb->all_pieces, F8);
+                break;
+            case C8: // Black Queenside
+                setbit(bb->rooks[BLACK], A8);
+                clearbit(bb->rooks[BLACK], D8);
+                setbit(bb->occupied[BLACK], A8);
+                clearbit(bb->occupied[BLACK], D8);
+                setbit(bb->all_pieces, A8);
+                clearbit(bb->all_pieces, D8);
+                break;
+        }
+    }
+}
