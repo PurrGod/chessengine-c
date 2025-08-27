7959c89ab4d5b49a0aa897e5c11ec2de21a0c6be
PurrGod <srinid369@gmail.com>
2025-08-26 20:45:52 -0700

    made code concise, removed switch statement to use a bitboard function. Moving to unmake.

--- src/make_moves.c
+++ src/make_moves.c
@@ -54,60 +54,11 @@ void make_move(Bitboards * bb, int m) {
     // piece that is moving, we can get that by using the from parameter
     // use the switch statement to then retrieve the bb array
     int movinPiece = get_piece_on_square(bb, from, side);
+    U64 * piece_bb = get_piece_bitboard(bb, movinPiece);
 
-    switch(movinPiece) {
-        case wPawn:
-            clearbit(bb->pawns[WHITE], from);
-            setbit(bb->pawns[WHITE], to);
-            break;
-        case wKnight:
-            clearbit(bb->knights[WHITE], from);
-            setbit(bb->knights[WHITE], to);
-            break;
-        case wBishop:
-            clearbit(bb->bishops[WHITE], from);
-            setbit(bb->bishops[WHITE], to);
-            break;
-        case wRook:
-            clearbit(bb->rooks[WHITE], from);
-            setbit(bb->rooks[WHITE], to);
-            break;
-        case wQueen:
-            clearbit(bb->queens[WHITE], from);
-            setbit(bb->queens[WHITE], to);
-            break;
-        case wKing:
-            clearbit(bb->kings[WHITE], from);
-            setbit(bb->kings[WHITE], to);
-            break;
-
-        // BLACK pieces
-        case bPawn:
-            clearbit(bb->pawns[BLACK], from);
-            setbit(bb->pawns[BLACK], to);
-            break;
-        case bKnight:
-            clearbit(bb->knights[BLACK], from);
-            setbit(bb->knights[BLACK], to);
-            break;
-        case bBishop:
-            clearbit(bb->bishops[BLACK], from);
-            setbit(bb->bishops[BLACK], to);
-            break;
-        case bRook:
-            clearbit(bb->rooks[BLACK], from);
-            setbit(bb->rooks[BLACK], to);
-            break;
-        case bQueen:
-            clearbit(bb->queens[BLACK], from);
-            setbit(bb->queens[BLACK], to);
-            break;
-        case bKing:
-            clearbit(bb->kings[BLACK], from);
-            setbit(bb->kings[BLACK], to);
-            break;        
-    }
-
+    clearbit(*piece_bb, from);
+    clearbit(*piece_bb, to);
+    
     // no matter what move, the from and to bits 
     // have to be updated and the xor needs to done
     // update hash
